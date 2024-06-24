e6c66537e6270fadc42cc3abf47a74b91ea46760
YellowCat98 <elcucuyisgay@gmail.com>
2024-06-24 14:25:46 +0100

    use tasks api

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,24 @@
 #include <Geode/modify/PlayerObject.hpp>
 #include <sstream>
 #include <fstream>
+#include <Geode/loader/Event.hpp>
 
 using namespace geode::prelude;
 namespace py = pybind11;
+
+using PyScript = Task<uint64_t, int>;
+
+PyScript runScript(std::string code) {
+    return PyScript::run([code](auto progress, auto called) -> PyScript::Result {
+        try {
+            py::exec(code);
+            return 0;
+        } catch (const py::error_already_set& e) {
+            log::info("Python Error:\n{}", e.what());
+            return 1;
+        }
+    }, "Run the python script idk what to tell you");
+}
 
 std::string getPyScript() {
     auto path = Mod::get()->getSettingValue<std::filesystem::path>("select-pyfile");
@@ -27,6 +42,10 @@ std::string getPyScript() {
 }
 
 class $modify(PlayerObject) {
+struct Fields {
+    EventListener<PyScript> gameListener;
+};
+
     void playerDestroyed(bool p0) {
 
         py::scoped_interpreter guard{};
@@ -43,11 +62,10 @@ print(value)
         } else {
             code = getPyScript();
         }
-        try {
-            py::exec(code);
-        } catch (const py::error_already_set& e) {
-            log::info("{}", e.what());
-        }
+        m_fields->gameListener.bind([](PyScript::Event* event) {
+            log::info("Progressing (or not)");
+        });
+        m_fields->gameListener.setFilter(runScript(code));
         PlayerObject::playerDestroyed(p0);
         
     }
