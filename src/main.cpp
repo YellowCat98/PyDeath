8f80ca53e17b8f982013c128a59317326d3221cc
YellowCat98 <elcucuyisgay@gmail.com>
2024-06-25 16:12:56 +0100

    simplify code, add more stuff to the task and HOIPEFIULLY it works

--- src/main.cpp
+++ src/main.cpp
@@ -16,13 +16,34 @@
 using namespace geode::prelude;
 namespace py = pybind11;
 
-using PyScript = Task<uint64_t, int>;
+using PyScript = Task<int, int>;
 
-PyScript runScript(std::string code) {
-    return PyScript::run([code](auto progress, auto called) -> PyScript::Result {
+std::string getPyScript() {
+    auto path = Mod::get()->getSettingValue<std::filesystem::path>("select-pyfile");
+    std::ifstream pyscript(path);
+
+    if (!pyscript) {
+std::string theCode = R"(
+import random
+list = ["You are dead!", "You have DIED...", "the PlayerObject::playerDestroyed function has been called"]
+
+value = random.choice(list)
+print(value)
+)";
+        return theCode;
+    }
+    std::stringstream buffer;
+    buffer << pyscript.rdbuf();
+    return buffer.str();
+}
+
+PyScript runScript() {
+    return PyScript::run([](auto progress, auto called) -> PyScript::Result {
+        std::string code;
+        code = getPyScript();
         try {
-            
-                py::exec(code);
+            py::scoped_interpreter guard{};
+            py::exec(code);
         } catch (const py::error_already_set& e) {
             log::info("Python Error:\n{}", e.what());
         }
@@ -30,37 +51,13 @@ PyScript runScript(std::string code) {
     });
 }
 
-std::string getPyScript() {
-    auto path = Mod::get()->getSettingValue<std::filesystem::path>("select-pyfile");
-    std::ifstream pyscript(path);
-    if (!pyscript) {
-        return "";
-    }
-    std::stringstream buffer;
-    buffer << pyscript.rdbuf();
-    return buffer.str();
-}
-
 class $modify(PlayerObject) {
 struct Fields {
     EventListener<PyScript> gameListener;
 };
     void playerDestroyed(bool p0) {
-        py::scoped_interpreter guard{}; // lets initialize the interpreter BEFORE running the task!!!
-        std::string code;
-        if (getPyScript().empty()) {
-            code = R"(
-import random
-list = ["You are dead!", "You have DIED...", "the PlayerObject::playerDestroyed function has been called"]
-
-value = random.choice(list)
-print(value)
-            )";
-        } else {
-            code = getPyScript();
-        }
         m_fields->gameListener.bind([](PyScript::Event* event){});
         
-        m_fields->gameListener.setFilter(runScript(code));
+        m_fields->gameListener.setFilter(runScript());
     }
 };
